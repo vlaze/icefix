8c12fdd8ec4a6a358ca83941f0dd0f5f1b6573a4
xvlaze <cptvlaze@tutamail.com>
2016-02-27 12:28:38 +0100

    syspref.js
    This is the preference file for the Icecat browser. It is expected to be stored in /etc/icecat.

---
+++ syspref.js
@@ -0,0 +1,9 @@
+// This file can be used to configure global preferences for IceCat
+// Example: Homepage
+//pref("browser.startup.homepage", "http://www.weebls-stuff.com/wab/");
+
+//This changes the 'GetMoreThemes' URL
+
+pref("lightweightThemes.getMoreURL", "https://addons.mozilla.org/%LOCALE%/firefox/themes");
+
+//You can use whatever URL you like, such as http://www.gnu.org/software/gnuzilla/addons.html#themes
